e5cbaef52484599bfdb7d28635560cf417c3837c
Michael A Ellis <69445443+michaelaellis@users.noreply.github.com>
2020-11-27 15:53:52 -0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,21 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="ellismikedesign@gmailcom";
+    $subject="$sender wants to chat!";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
